ea2f8a528542f1a3280b9b680b8834e9b9579d0a
ngarnsworthy <ngarnsworthy@gmail.com>
2018-10-26 20:24:03 -0500

    add

--- Advanced Economies/js/js.js
+++ Advanced Economies/js/js.js
@@ -1,26 +1,31 @@
 var startMoney = 0
 
-var plaerOne = startMoney,
-    plaerTwo = startMoney,
-    plaerThree = startMoney,
-    plaerFour = startMoney,
-    MortgageRate
+var plaerOne = startMoney
+var plaerTwo = startMoney
+var plaerThree = startMoney
+var plaerFour = startMoney
+var MortgageRate = getElementById('idMortgageRate')
+var tax = getElementById('idTax')
 
 function start() {
-  MortgageRate = document.getElementById('idMortgageRate').value - 0
   startMoney = document.getElementById("idStartAcountAmount").value - 0
   plaerOne = startMoney
   plaerTwo = startMoney
   plaerThree = startMoney
   plaerFour = startMoney
+  MortgageRate = getElementById('idMortgageRate')
+  tax = getElementById('idTax')
   swichAndReset()
 }
 
 
 function swichAndReset() {
+
   document.getElementById('plaerOne').innerHTML = plaerOne;
   document.getElementById('plaerTwo').innerHTML = plaerTwo;
   document.getElementById('plaerThree').innerHTML = plaerThree;
   document.getElementById('plaerFour').innerHTML = plaerFour;
   document.getElementById('idMortgageRateDiv').innerHTML = MortgageRate;
+  document.getElementById('idTax').innerHTML = idTaxDiv;
+
 }
