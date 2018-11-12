7f8a925c8ceac11a4d2cc569f537075a8b11c304
ngarnsworthy <ngarnsworthy@gmail.com>
2018-11-11 20:59:20 -0600

    ud

--- Advanced Economies/js/js.js
+++ Advanced Economies/js/js.js
@@ -1,26 +1,42 @@
 var startMoney = 0
 
-var plaerOne = startMoney,
-    plaerTwo = startMoney,
-    plaerThree = startMoney,
-    plaerFour = startMoney,
-    MortgageRate
+var plaerOne = startMoney
+var plaerTwo = startMoney
+var plaerThree = startMoney
+var plaerFour = startMoney
+var MortgageRate
+var tax
+
+// getElement shortcut
+// returns the element
+function ge(id) {
+  return document.getElementById(id);
+}
 
 function start() {
-  MortgageRate = document.getElementById('idMortgageRate').value - 0
-  startMoney = document.getElementById("idStartAcountAmount").value - 0
+  startMoney = ge("idStartAcountAmount").value - 0
   plaerOne = startMoney
   plaerTwo = startMoney
   plaerThree = startMoney
   plaerFour = startMoney
+  MortgageRate = ge('idMortgageRate').value - 0
+  tax = ge('idTax').value - 0
   swichAndReset()
 }
 
 
 function swichAndReset() {
-  document.getElementById('plaerOne').innerHTML = plaerOne;
-  document.getElementById('plaerTwo').innerHTML = plaerTwo;
-  document.getElementById('plaerThree').innerHTML = plaerThree;
-  document.getElementById('plaerFour').innerHTML = plaerFour;
-  document.getElementById('idMortgageRateDiv').innerHTML = MortgageRate;
+
+  ge('plaerOne').innerHTML = plaerOne;
+  ge('plaerTwo').innerHTML = plaerTwo;
+  ge('plaerThree').innerHTML = plaerThree;
+  ge('plaerFour').innerHTML = plaerFour;
+  ge('idMortgageRateDiv').innerHTML = MortgageRate;
+  ge('idTaxDiv').innerHTML = tax;
+
+}
+
+function changeValue(id) {
+  var value = ge('id'+id+'Change').value-0;
+  ge('id' + id + 'Div').innerHTML = value;
 }
